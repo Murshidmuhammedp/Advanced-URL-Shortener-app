d44d8f752055b60ae24a34219b29fb501cc4dce5
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-12-31 16:05:39 +0530

    URL model

---
+++ Models/UrlModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+    longUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true
+    },
+    alias: {
+        type: String,
+        unique: true
+    },
+    topic: {
+        type: String
+    }, createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    clicks: [{
+        timestamp: {
+            type: Date,
+            default: Date.now
+        },
+        userAgent: String,
+        ipAddress: String,
+        geolocation: Object
+    }]
+});
+
+const URL = mongoose.model("URL", urlSchema);
+
+export default URL;
